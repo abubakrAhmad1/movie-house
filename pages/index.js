a6fb6f3c17c07a7e2f37a0c67591b077bd683b29
abubakrAhmad1 <abubakarahmad0045@gmail.com>
2025-04-22 03:48:54 -0700

    feat: home page addition

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
+
+export default function Home({ data }) {
+
+  function handle() {
+
+  }
+  const trendingMovies = data.movies.filter(movie => movie.rating >= 8.5);
+  return (
+    <div>
+      <h2>TRENDING MOVIES..!!!</h2>
+      {trendingMovies.map((movie) => (
+        <div>{movie.title}</div>
+      ))}
+      <button onClick={handle}>Browse Generes</button>
+    </div>
+  );
+}
 
 export async function getStaticProps() {
-  const path = path.join(process.cwd(), 'data','data.json');
-  const fileData = fs.readFileSync(path ,'utf-8');
+  const filePath = path.join(process.cwd(), "data", "data.json");
+  const fileData = fs.readFileSync(filePath, "utf-8");
   const finalData = JSON.parse(fileData);
+  return {
+    props: {
+      data: finalData,
+    },
+  };
 }
