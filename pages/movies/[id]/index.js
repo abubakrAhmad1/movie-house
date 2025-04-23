b0e64efd6c7643d34335c37dd40beeb7476d8aff
abubakrAhmad1 <abubakarahmad0045@gmail.com>
2025-04-23 08:40:17 -0700

    feat : movies page

--- pages/movies/[id]/index.js
+++ pages/movies/[id]/index.js
@@ -8,8 +8,36 @@ export async function getStaticPaths() {
 
   return {
     //params should be an array of objects with type => {params : {id : value}}
-    params: data.movies.map((movie) => (
-        {id : movie.id}
-    )),
+    paths: data.movies.map((movie) => ({
+      params: { id: movie.id.toString() },
+    })),
+    fallback: false,
   };
+}
+
+export async function getStaticProps({ params }) {
+  const filePath = path.join(process.cwd(), "data", "data.json");
+  const fileData = fs.readFileSync(filePath, "utf-8");
+  const data = JSON.parse(fileData);
+
+  const selectedMovie = data.movies.find(
+    (movie) => movie.id.toString() === params.id
+  );
+
+  return {
+    props: {
+      movie: selectedMovie,
+    },
+  };
+}
+
+export default function Discription({ movie }) {
+  return (
+    <div>
+      <h2>{movie.title}</h2>
+      <p>{movie.description}</p>
+      <p>{movie.releaseYear}</p>
+      <p>{movie.rating}</p>
+    </div>
+  );
 }
