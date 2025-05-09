f0b141df4f79b6628077e6e1fd3c57e8bc2ef2e3
abubakrAhmad1 <abubakarahmad0045@gmail.com>
2025-05-09 11:15:02 -0700

    feat : toggle button added

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,21 @@ import fs from "fs";
 import { notFound } from "next/navigation";
 import { useRouter } from "next/router";
 import path from "path";
+import { useTheme } from "../context/ThemeContext";
 
 export default function Home({ data }) {
   const router = useRouter();
+  const { theme, toggleTheme } = useTheme();
 
   function handle() {
-    router.push('/genre');
+    router.push("/genre");
   }
-  const trendingMovies = data.movies.filter(movie => movie.rating >= 8.5);
+  const trendingMovies = data.movies.filter((movie) => movie.rating >= 8.5);
   return (
     <div>
+      <button onClick={toggleTheme}>
+        Switch to {theme === "light" ? "Dark" : "Light"} Mode
+      </button>
       <h2>TRENDING MOVIES..!!!</h2>
       {trendingMovies.map((movie) => (
         <div>{movie.title}</div>
@@ -25,15 +30,15 @@ export async function getStaticProps() {
   const filePath = path.join(process.cwd(), "data", "data.json");
   const fileData = fs.readFileSync(filePath, "utf-8");
   const finalData = JSON.parse(fileData);
-  if(!finalData) {
+  if (!finalData) {
     return {
-      notFound : true
-    }
+      notFound: true,
+    };
   }
   return {
     props: {
       data: finalData,
     },
-    revalidate : 10,
+    revalidate: 10,
   };
 }
